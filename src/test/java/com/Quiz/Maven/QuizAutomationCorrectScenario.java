4cacf18d0f78290865b652375ec1ab800710a4e4
Geetikageet <56255889+Geetikageet@users.noreply.github.com>
2020-03-18 12:26:35 +0000

    Add files via upload

---
+++ src/test/java/com/Quiz/Maven/QuizAutomationCorrectScenario.java
@@ -0,0 +1,73 @@
+package com.Quiz.Maven;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class QuizAutomationCorrectScenario {
+	public WebDriver driver;
+	@Test
+	public void test() {
+		System.setProperty("webdriver.chrome.driver" ,"/Users/geetika/Desktop/Automation/MAC/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.get("https://campaignstaging.odicci.com/#/e07c82c9347a655f24aa");
+		driver.findElement(By.name("Email")).click();
+		driver.findElement(By.name("Email")).sendKeys("sd@ss.com");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.findElement(By.id("516")).click();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.findElement(By.xpath("//*[@id=\"submitButton\"]/div")).click();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.findElement(By.cssSelector("#imageTagBox > div > div:nth-child(1) > div > div.imageTagElementImageWrapper.clickable > div > span")).click();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.findElement(By.xpath("//*[@id=\"submitButton\"]/div")).click();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.findElement(By.cssSelector("#textTagBox > div:nth-child(2) > div.textTagElementText")).click();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.findElement(By.xpath("//*[@id=\"submitButton\"]/div")).click();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
